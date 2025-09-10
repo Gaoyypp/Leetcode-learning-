75f0a3371b81d11ea4e7d065926e94853c9c6264
Gaoyypp <2628573253@qq.com>
2025-09-10 18:34:26 +0800

    Update 34.在排序数组中查找元素的第一个和最后一个位置.cpp

--- 34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ 34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left=0, right=nums.size()-1;
-        int l=-1,r=-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-//left
-            if(nums[mid]==target){
-                right=mid-1;
-            }else if(nums[mid]>target){
-                right=mid-1;
-            }else if(nums[mid]<target){
-                left=mid+1;
+        int left = 0, right = nums.size() - 1;
+        int l = -1, r = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // left
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
-        
-        l=(left<nums.size()&&nums[left]==target)?left:-1;
-//right
-        left=0,right=nums.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                left=mid+1;
-            }else if(nums[mid]>target){
-                right=mid-1;
-            }else if(nums[mid]<target){
-                left=mid+1;
+
+        l = (left < nums.size() && nums[left] == target) ? left : -1;
+        // right
+        left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
-        
-        r=(left>0&&nums[left-1]==target)?(left-1):-1;
-        
-        return {l,r};
+
+        r = (left > 0 && nums[left - 1] == target) ? (left - 1) : -1;
+
+        return {l, r};
     }
 };
