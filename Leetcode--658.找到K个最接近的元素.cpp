52666f5edd01da8cf4fcd148cd088b914f79a38c
Gaoyypp <2628573253@qq.com>
2025-09-14 12:27:01 +0800

    Create Leetcode--658.找到K个最接近的元素.cpp

---
+++ Leetcode--658.找到K个最接近的元素.cpp
@@ -0,0 +1,47 @@
+题目链接:
+https://leetcode.cn/problems/find-k-closest-elements/
+
+/*这道题目的精妙之处在于用了一个双端都开的区间,区间元素个数为right-left-1,
+通过限定下标的范围,在确定好最终的right 和 left之后再将元素插入数组res中,避免了先插入再排序的额外时间,
+在极端条件下,可能为O(n^2)的时间复杂度,算法效率非常地高!!!*/
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> res;
+        int p = left_bound(arr, x);
+        int left = p - 1;
+        int right = p;
+        // 两边都开的区间(left,right)内的元素个数为right-left-1
+        while (right - left - 1 < k) {
+            if (left == -1) {
+                right++;
+            } else if (right == arr.size()) {
+                left--;
+            } else if (abs(arr[left] - x) > abs(arr[right] - x)) {
+                right++;
+            } else {
+                left--;
+            }
+        }
+
+        for (int i = left + 1; i < right; i++) {
+            res.push_back(arr[i]);
+        }
+        return res;
+    }
+
+    int left_bound(vector<int>& arr, int target) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
